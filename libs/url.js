0ddbfeefdec4d896ee4a8d1c4ea6c4a7fb384f75
cgxqd <1661269978@qq.com>
2019-05-13 17:16:05 +0800

    url序列化和反序列化函数 URLSearchParams

--- libs/url.js
+++ libs/url.js
@@ -1,15 +1,55 @@
+import { $isObject, $isString } from './types'
 /**
  * 请求参数
  * @param {*} url 
- * @param {*} options 
+ * @param {Object} options 
+ * @example Url('http://www.baidu.com?:name',{name:'ceshi'}) // => http://www.baidu.com?ceshi
  */
-export const Url = (url, options={})=>{
-	return url.replace(/:([a-zA-Z0-9_]{1,})/g,($0,$1)=>{
-		let val = encodeURIComponent(options[$1]);
-		if(val === undefined){
-			new Error(`URL ${url} not find ${$1}`);
-		}
-		return  val;
-	})
+export const Url = (url, options = {}) => {
+    return url.replace(/:([a-zA-Z0-9_]{1,})/g, ($0, $1) => {
+        let val = encodeURIComponent(options[$1]);
+        if (val === undefined) {
+            new Error(`URL ${url} not find ${$1}`);
+        }
+        return val;
+    })
 }
-	
+
+
+/**
+ * url 序列化和反序列化
+ * @param {Object||String} param 
+ * @example URLSearchParams('https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=parseQueryString&rsv_pq=8c7a6d0000146171&rsv_t=43d6RzTiyjUjUKtQtqfR3XL25JepKFwJYvvSpsj%2BJ7aFqxdBLDungY%2Bfx%2BE&rqlang=cn&rsv_enter=1&rsv_n=2&rsv_sug3=1') // => 
+ *  // {
+    //    "ie":"utf-8",
+    //    "f":"8",
+    //    "rsv_bp":"1",
+    //    "rsv_idx":"1",
+    //    "tn":"baidu",
+    //    "wd":"parseQueryString",
+    //    "rsv_pq":"8c7a6d0000146171",
+    //    "rsv_t":"43d6RzTiyjUjUKtQtqfR3XL25JepKFwJYvvSpsj+J7aFqxdBLDungY+fx+E",
+    //    "rqlang":"cn",
+    //    "rsv_enter":"1",
+    //    "rsv_n":"2",
+    //    "rsv_sug3":"1"
+    // }
+ *  // utilscore.URLSearchParams({
+    // 	name:'cgx',
+    // 	test:'ceshi'
+    // })
+    // // => "name=cgx&test=ceshi"
+ */
+
+export const URLSearchParams = (param) => {
+    if ($isObject(param)) {
+        return Object.keys(param).map(key => `${key}=${encodeURIComponent(param[key])}`).join('&')
+    } else if ($isString(param)) {
+        let maps = {};
+        param.replace(/^.[^\?]*\?/g, '').split('&').forEach(res => {
+            let row = decodeURIComponent(res).split('=');
+            maps[row[0] + ''] = decodeURIComponent(row[1])
+        });
+        return maps
+    }
+}
