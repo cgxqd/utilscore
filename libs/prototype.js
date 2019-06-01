d299b2202d013582338d0d7909207b8574ab6263
cgxqd <1661269978@qq.com>
2019-06-01 10:57:57 +0800

    refactor：重构原型扩展方法 Object.fromEntries

--- libs/prototype.js
+++ libs/prototype.js
@@ -1,22 +1,26 @@
 //目前es10 的 matchAll 缺点是：1、要将迭代器转换为数组。2、需要设置/g
-String.prototype.match_all = function(reg){
+String.prototype.match_all = function (reg) {
 	var arr = []
 	var str = this;
 	var s = '';
 	while ((s = reg.exec(str)) != null) {
 		arr.push(s[1])
-	} 
+	}
 	return arr
-}	
+}
 
 // 低于chrome74版本的需要兼容
-Object.prototype.fromEntries = function(val){
-	return val.reduce((prev,val)=>Object.assign(prev,{[val[0]]:val[1]}),{})
+Object.prototype.fromEntries = function (arr) {
+	var o = {}
+	for (let k of arr) {
+		o[k[0]] = k[1]
+	}
+	return o
 }
 
 // 兼容性还可以
-Object.prototype.entries = function(obj){
-	return Object.keys(obj).reduce((acc,target)=>[...acc,[target,obj[target]]],[])
+Object.prototype.entries = function (obj) {
+	return Object.keys(obj).reduce((acc, target) => [...acc, [target, obj[target]]], [])
 }
 
 
