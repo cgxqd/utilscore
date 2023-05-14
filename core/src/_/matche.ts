cf79e2c243f10107be1a777738abb2f6e114e387
cgxqd <1661269978@qq.com>
2023-05-14 17:22:15 +0800

    fix(refactor): isEmpty

--- core/src/_/matche.ts
+++ core/src/_/matche.ts
@@ -1,16 +1,14 @@
 import { ARRAY_MAX_SAFE_INTEGER } from "./const";
 /**
  * 获取类型 Tag
- * @param {any} value
- * @returns {string}
+ * @param value
  */
 export const getTypeTag = (value: any): string =>
   Object.prototype.toString.call(value);
 
 /**
  * 获取类型
- * @param {any} value
- * @returns {string}
+ * @param value
  */
 export const getType = (value: any): string =>
   getTypeTag(value)
@@ -19,62 +17,54 @@ export const getType = (value: any): string =>
 
 /**
  * 判断类型是否 NaN
- * @param {any} value
- * @returns {boolean}
+ * @param value
  */
 export const isNaN = (value: any): boolean =>
   isNumber(value) && value !== +value;
 
 /**
  * 判断类型是否 Null
- * @param {any} value
- * @returns {value is null}
+ * @param value
  */
 export const isNull = (value: any): value is null => getType(value) === "null";
 
 /**
  * 判断类型是否 Undefined
- * @param {any} value
- * @returns {value is undefined}
+ * @param value
  */
 export const isUndefined = (value: any): value is undefined => value === void 0;
 
 /**
  * 判断类型是否 Boolean
- * @param {any} value
- * @returns {value is boolean}
+ * @param value
  */
 export const isBoolean = (value: any): value is boolean =>
   typeof value === "boolean";
 
 /**
  * 判断类型是否 BigInt
- * @param {any} value
- * @return {value is bigint}
+ * @param value
  */
 export const isBigInt = (value: any): value is bigint =>
   typeof value === "bigint";
 
 /**
  * 判断类型是否 Number
- * @param {any} value
- * @return {value is number}
+ * @param value
  */
 export const isNumber = (value: any): value is number =>
   typeof value === "number";
 
 /**
  * 判断类型是否 String
- * @param {any} value
- * @return {value is string}
+ * @param value
  */
 export const isString = (value: any): value is string =>
   typeof value === "string";
 
 /**
  * 判断类型是否 Symbol
- * @param {any} value
- * @returns {value is symbol}
+ * @param value
  */
 export const isSymbol = (value: any): value is symbol =>
   typeof value == "symbol" ||
@@ -82,16 +72,14 @@ export const isSymbol = (value: any): value is symbol =>
 
 /**
  * 判断是不是纯粹的对象
- * @param {any} value
- * @returns {value is object}
+ * @param value
  */
 export const isPrimeObject = (value: any): value is object =>
   getType(value) === "object";
 
 /**
  * 判断是否 Object
- * @param {any} value
- * @returns {boolean}
+ * @param value
  */
 export const isObject = (value: any): boolean => {
   const type = typeof value;
@@ -100,38 +88,33 @@ export const isObject = (value: any): boolean => {
 
 /**
  * 判断类型是否 RegExp
- * @param {any} value
- * @returns {value is RegExp}
+ * @param value
  */
 export const isRegExp = (value: any): value is RegExp =>
   getType(value) === "regexp";
 
 /**
  * 判断类型是否 Date
- * @param {any} value
- * @returns {value is Date}
+ * @param value
  */
 export const isDate = (value: any): value is Date => getType(value) === "date";
 
 /**
  * 判断类型是否 Array
- * @param {any} value
- * @returns {value is []}
+ * @param value
  */
 export const isArray = (value: any): value is [] => Array.isArray(value);
 
 /**
  * 判断类型是否 Function
- * @param {any} value
- * @returns {value is Function}
+ * @param value
  */
 export const isFunction = (value: any): value is () => void =>
   typeof value === "function";
 
 /**
  * 判断类型是否 Promise
- * @param {any} value
- * @returns {value is Promise}
+ * @param value
  */
 export const isPromise = (value: any): value is Promise<any> => {
   if (Promise && Promise.resolve) {
@@ -142,16 +125,14 @@ export const isPromise = (value: any): value is Promise<any> => {
 
 /**
  * 判断是否整数
- * @param {any} value
- * @returns {boolean}
+ * @param value
  */
 export const isInteger = (value: any): boolean => value % 1 === 0;
 
 /**
  * 数值是否在有效的数组安全长度范围
  * 大于等于0并且小于MAX_SAFE_INTEGER
- * @param {any} value
- * @returns {boolean}
+ * @param value
  */
 export const isArraySafeLength = (value: any): boolean =>
   isNumber(value) && value >= 0 && value < ARRAY_MAX_SAFE_INTEGER;
@@ -161,8 +142,7 @@ export const isArraySafeLength = (value: any): boolean =>
  * 1：不为null, undefined
  * 2：为object
  * 3: length属性为数组安全长度
- * @param {any} value
- * @returns {boolean}
+ * @param value
  */
 export const isArrayLike = (value: any): boolean => {
   return (
@@ -175,27 +155,43 @@ export const isArrayLike = (value: any): boolean => {
 
 /**
  * 判断元素是否为空
- * @param {any} value
- * @returns {boolean}
+ * @param value
  */
 export const isEmpty = (value: any): boolean => {
   if (value == null) return true;
-  else if (value === "object") return !Object.keys(value).length;
-  else if (isArray(value)) return !value.length;
-  else if (isString(value)) return !value;
-  else return value.toString().length == 0;
+  if (isBoolean(value)) return !value;
+  if (isNumber(value)) return !value;
+  if (value instanceof Error) return value.message === "";
+  switch (getType(value)) {
+    case "function":
+    case "string":
+    case "array": {
+      return !value.length;
+    }
+  }
+  if (typeof value === "object") {
+    if (hasIterable && typeof value[Symbol.iterator] === "function") {
+      let size = 0;
+      value.forEach(() => size++);
+      return !size;
+    }
+    return !Object.keys(value).length;
+  }
+
+  return false;
 };
 
 /**
  * 判断对象自身属性中是否具有指定的属性
- * @template {Record<string, any>} T
- * @param {T} target
- * @param {string} key 属性
- * @returns {boolean}
+ * @param target
+ * @param key 属性
  */
 export const hasOwn = <T extends Record<string, any>>(
   target: T,
   key: string
 ): boolean => Object.prototype.hasOwnProperty.call(target, key);
 
+/**
+ * 判断是否支持 iterator
+ */
 export const hasIterable = !!(typeof Symbol === "function" && Symbol.iterator);
